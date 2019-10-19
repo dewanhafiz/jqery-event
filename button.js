4068b34844372820d5a7192f9db8e4747d2e1df3
dewanhafiz <45536533+dewanhafiz@users.noreply.github.com>
2019-10-19 22:33:38 +0600

    Add files via upload

---
+++ button.js
@@ -0,0 +1,35 @@
+var redBtnElement = document.getElementById('red');
+redBtnElement.onclick = function () {
+
+    var divoneElement= document.getElementById('divone');
+    divoneElement.style.backgroundColor ='red';
+
+
+
+}
+
+var greenBtnElement = document.getElementById('green');
+greenBtnElement.onclick = function () {
+
+    var divoneElement= document.getElementById('divone');
+    divoneElement.style.backgroundColor ='green';
+
+}
+
+var greenBtnElement = document.getElementById('yellow');
+greenBtnElement.onclick = function () {
+
+    var divoneElement= document.getElementById('divone');
+    divoneElement.style.backgroundColor ='yellow';
+
+}
+
+var greenBtnElement = document.getElementById('dark');
+greenBtnElement.onclick = function () {
+    /*  var divoneElement = document.getElementById('divone');
+
+      divoneElement.className = '.red';*/
+    var divoneElement= document.getElementById('divone');
+    divoneElement.style.backgroundColor ='black';
+
+}
